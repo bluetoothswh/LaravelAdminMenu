49948d37a50de03a9b1c5840fd3463bcc085b635
David Sun <sunweihua@sunweihuadeiMac.local>
2017-10-26 12:06:53 +0800

    update config file

--- src/config/menu.php
+++ src/config/menu.php
@@ -70,19 +70,19 @@ return [
 									[
 										'name'=>'自定义导航栏',
 										'tag'=>'admin.nav.index',
-										'url'=>url('admin/nav'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 									[
 										'name'=>'整站图片管理',
 										'tag'=>'admin.image.index',
-										'url'=>url('admin/image'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 								    [
 										'name'=>'友情链接管理',
 										'tag'=>'admin.link.index',
-										'url'=>url('admin/link'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 					],
@@ -91,7 +91,7 @@ return [
 									[
 										'name'=>'系统信息',
 										'tag'=>'admin.system.index',
-										'url'=>url('admin/index'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 					],
@@ -99,78 +99,78 @@ return [
 									[
 										'name'=>'商品列表',
 										'tag'=>'admin.goods.index',
-										'url'=>url('admin/goods'),
+										'url'=>'',
 										'icon'=>'<i class="icon-present"></i>'
 								    ],
 								    [
 										'name'=>'商品分类',
 										'tag'=>'admin.category.index',
-										'url'=>url('admin/category'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 								    [
 										'name'=>'商品品牌',
 										'tag'=>'admin.brand.index',
-										'url'=>url('admin/brand'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 									[
 										'name'=>'商品类型管理',
 										'tag'=>'admin.type.index',
-										'url'=>url('admin/type'),
+										'url'=>'',
 								    ],
 								    [
 										'name'=>'属性管理',
 										'tag'=>'admin.attribute.index',
-										'url'=>url('admin/attribute'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 								    [
 										'name'=>'商品规格',
 										'tag'=>'admin.field.index',
-										'url'=>url('admin/field'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 								    [
 										'name'=>'颜色属性管理',
 										'tag'=>'admin.color.index',
-										'url'=>url('admin/color'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 								    [
 										'name'=>'属性链货品管理',
 										'tag'=>'admin.product.index',
-										'url'=>url('admin/product'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 								    [
 										'name'=>'Excel导入导出',
 										'tag'=>'admin.excel.index',
-										'url'=>url('admin/excel'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 								    [
 										'name'=>'命令行批量添加商品',
 										'tag'=>'admin.command.index',
-										'url'=>url('admin/command'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 								    [
 										'name'=>'商品图片批量处理',
 										'tag'=>'admin.goods.image',
-										'url'=>url('admin/goods/image'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 								    [
 										'name'=>'标签管理',
 										'tag'=>'admin.tag.index',
-										'url'=>url('admin/tag'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 								    [
 										'name'=>'商品回收站',
 										'tag'=>'admin.cycle.index',
-										'url'=>url('admin/cycle'),
+										'url'=>'',
 										'icon'=>'<i class="fa fa-home"><i>'
 								    ],
 					],
@@ -186,88 +186,81 @@ return [
 										[
 											'name'=>'订单列表',
 											'tag'=>'admin.order.index',
-											'url'=>url('admin/order'),
-											'icon'=>'',
+											'url'=>'',
 
 										],
 										[
 											'name'=>'添加订单',
 											'tag'=>'admin.order.create',
-											'url'=>url('admin/order/create'),
+											'url'=>'',
 										],
 										[
 											'name'=>'订单日志',
 											'tag'=>'admin.order.log',
-											'url'=>url('admin/order/log'),
+											'url'=>'',
 										],
 
 										[
 											'name'=>'发货单管理',
 											'tag'=>'admin.express.index',
-											'url'=>url('admin/express'),
+											'url'=>'',
 										],
 
 										[
 											'name'=>'退货管理',
 											'tag'=>'admin.return.index',
-											'url'=>url('admin/return'),
+											'url'=>'',
 										],
 										[
 											'name'=>'订单打印',
 											'tag'=>'admin.order.print',
-											'url'=>url('admin/order/print'),
+											'url'=>'',
 										],
 					],
 					'user' =>[
 								[
 										'name'=>'用户列表',
 										'tag'=>'admin.user.index',
-										'url'=>url('admin/user'),
-										'icon'=>'<i class="icon-present"></i>'
+										'url'=>'',
 								],
 							    [
 									'name'=>'会员等级',
 									'tag'=>'admin.user_rank.index',
-									'url'=>url('admin/user_rank'),
-									'icon'=>'<i class="fa fa-home"><i>'
+									'url'=>'',
 							  ],
 							  [
 									'name'=>'充值和提现',
 									'tag'=>'admin.account.index',
-									'url'=>url('admin/account'),
-									'icon'=>'<i class="fa fa-home"><i>'
+									'url'=>'',
 							  ],
 							  [
 									'name'=>'会员留言',
 									'tag'=>'admin.message.index',
-									'url'=>url('admin/message'),
-									'icon'=>'<i class="fa fa-home"><i>'
+									'url'=>'',
 							  ],
 							  [
 									'name'=>'短消息管理',
 									'tag'=>'admin.sms.index',
-									'url'=>url('admin/sms'),
-									'icon'=>'<i class="fa fa-home"><i>'
+									'url'=>'',
 							  ],
 							  [
 									'name'=>'演示账号管理',
 									'tag'=>'admin.demo.index',
-									'url'=>url('admin/demo'),
-									'icon'=>'<i class="fa fa-home"><i>'
+									'url'=>'',
 							  ],
 					],
 					'article'	 =>[
 										[
 											'name'=>'文章列表',
 											'tag'=>'admin.article.index',
-											'url'=>url('admin/article'),
+											'url'=>'',
 											'icon'=>'<i class="fa fa-cogs"></i>'
 										],
 
 										[
 											'name'=>'文章分类',
 											'tag'=>'admin.article_cat.index',
-											'url'=>url('admin/article_cat'),
+											'url'=>'',
 											'icon'=>'<i class="fa fa-cogs"></i>'
 										],
 					],
@@ -275,7 +268,7 @@ return [
 										[
 											'name'=>'供货商列表',
 											'tag'=>'admin.supplier.index',
-											'url'=>url('admin/supplier'),
+											'url'=>'',
 											'icon'=>'<i class="fa fa-cogs"></i>'
 										],				
 					],
@@ -283,101 +276,87 @@ return [
 										[
 											'name'=>'管理员列表',
 											'tag'=>'admin.administrator.index',
-											'url'=>url('admin/administrator'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 
 										[
 											'name'=>'系统所有权限清单',
 											'tag'=>'admin.privi.index',
-											'url'=>url('admin/privi'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 										[
 											'name'=>'角色管理',
 											'tag'=>'admin.role.index',
-											'url'=>url('admin/role'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 
 										[
 											'name'=>'日志管理',
 											'tag'=>'admin.log.index',
-											'url'=>url('admin/log'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 
 
 										[
 											'name'=>'退出登录',
 											'tag'=>'admin.administrator.logout',
-											'url'=>url('admin/administrator/logout'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 					],
 					'template' =>[
 						                [
 											'name'=>'模板设置',
 											'tag'=>'admin.template.index',
-											'url'=>url('admin/template'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 										[
 											'name'=>'模板颜色选择器',
 											'tag'=>'admin.style.index',
-											'url'=>url('admin/style'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 								],
 		            'config' =>[
 						                [
 											'name'=>'商城系统设置',
 											'tag'=>'admin.config.index',
-											'url'=>url('admin/config'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 
 										[
 											'name'=>'首页幻灯片设置',
 											'tag'=>'admin.slider.index',
-											'url'=>url('admin/slider'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 
 										[
 											'name'=>'分类广告管理',
 											'tag'=>'admin.catad.index',
-											'url'=>url('admin/catad'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 
 										[
 											'name'=>'支付方式',
 											'tag'=>'admin.payment.index',
-											'url'=>url('admin/payment'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 
 										[
 											'name'=>'配送方式',
 											'tag'=>'admin.shipping.index',
-											'url'=>url('admin/shipping'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 
 										[
 											'name'=>'地区运费设置',
 											'tag'=>'admin.region_shipping.index',
-											'url'=>url('admin/region_shipping'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 					],
 					'mobile' =>[
 						                [
 											'name'=>'wap版本基本设置',
 											'tag'=>'admin.wap.index',
-											'url'=>url('admin/wap'),
-											'icon'=>'<i class="fa fa-cogs"></i>'
+											'url'=>'',
 										],
 					],
 	],
